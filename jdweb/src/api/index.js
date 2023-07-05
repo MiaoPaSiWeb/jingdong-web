b9cc5216bcc40a58ca02b6a81ed76fc910fa3e55
zhuyuhui <zhuyuhui@odianyun.com>
2023-07-05 09:40:17 +0800

    feat: 添加mock数据

---
+++ jdweb/src/api/index.js
@@ -0,0 +1,19 @@
+/*
+广告组件
+    mock/home/jsonpBackground.json
+
+Shortcut组件
+    网站导航弹出框数据：mock/home/jsonpshortcutSitenav.json
+    企业采购弹出狂数据：mock/home/jsonpshortcutCompanyConfig.json
+Header组件
+    mock/home/jsonpHotWords.json
+
+
+首页路由组件
+    轮播 mock/home/jsonpFocus.json
+    左侧分类 mock/home/getCategoryCallback.json
+    秒杀数据   mock/home/pc_seckill.json
+    频道广场 mock/home/jsonpMarket.json
+    京东快报 mock/home/jsonpNews.json
+    为你推荐 mock/home/pc_home_feed/result_{$page}.json   page 从 1 开始
+*/
